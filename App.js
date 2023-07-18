29375935b1dcc98afaeea78c0e85a82bd7d4a23a
webixr <webixr1@gmail.com>
2023-07-18 14:45:19 +0300

    first commit, and add navbar with full features

---
+++ App.js
@@ -0,0 +1,14 @@
+// menu start
+const open = document.getElementById("menuOpenBtn");
+const menu = document.getElementById("menu");
+const close = document.getElementById("menuCloseBtn");
+
+open.addEventListener("click", () => {
+    menu.classList.remove("hidden", "-translate-x-80", "opacity-0")
+    menu.classList.add("flex", "-translate-x-0", "opacity-100")
+})
+close.addEventListener("click", () => {
+    menu.classList.remove("flex", "-translate-x-0", "opacity-100")
+    menu.classList.add("hidden", "-translate-x-80", "opacity-0")
+})
+// menu end
