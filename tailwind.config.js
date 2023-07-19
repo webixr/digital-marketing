830895a76d1a9689cca01c7a6a2b3508bef1a217
webixr <webixr1@gmail.com>
2023-07-19 20:38:03 +0300

    add header, brands and strategic sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        ltr: {
+          "0%, 100%": {
+            transform: "translateX(-25%)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateX(0)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
+      animation: {
+        ltr: "ltr 1s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
